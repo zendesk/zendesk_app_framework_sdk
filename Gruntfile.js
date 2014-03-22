7e56e241053c36022699739e2811ff4722e876c9
Daniel Ribeiro <djbreves@gmail.com>
2014-03-22 20:21:26 +0800

    Add Gluejs, Mocha, and Uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,42 @@ module.exports = function (grunt) {
         src: ['lib/**/*.js']
       },
       test: {
-        src: ['test/**/*.js']
+        src: ['spec/**/*.js']
+      }
+    },
+    gluejs: {
+      options: {
+        export: 'ZAFClient'
+      },
+      build: {
+        src: 'lib/**/*.js',
+        dest: 'build/zaf_client.js'
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'build/zaf_client.min.js': ['build/zaf_client.js']
+        }
+      }
+    },
+    mochaRunner: {
+      all: {
+        scripts: [
+          'build/zaf_client.js',
+          'spec/**/*.js'
+        ]
+      }
+    },
+    mocha: {
+      options: {
+        run: true,
+        reporter: 'Spec',
+      },
+      test: {
+        options: {
+          urls: ['http://localhost:8000']
+        }
       }
     },
     watch: {
@@ -39,6 +74,8 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'gluejs', 'mochaRunner', 'mocha']);
+  grunt.registerTask('build', ['jshint', 'gluejs', 'uglify']);
+  grunt.registerTask('default', ['build']);
 
 };
