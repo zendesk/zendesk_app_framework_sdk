10bf3ea59b52caf6daa6096b67c30f98093c6a3a
Stefan Vizzari <svizzari@zendesk.com>
2014-04-24 11:30:03 +1000

    Add tests for client

---
+++ spec/client_spec.js
@@ -0,0 +1,123 @@
+describe.only('Client', function() {
+
+  var Client  = require('client'),
+      sandbox = sinon.sandbox.create(),
+      origin  = 'foo.zendesk.com',
+      appGuid = 'ABC123',
+      postMessage,
+      subject;
+
+  beforeEach(function() {
+    sandbox.stub(window.top, 'postMessage');
+    subject = new Client(origin, appGuid);
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  it('can be instantiated', function() {
+    expect(subject).to.exist;
+  });
+
+  it('posts an "iframe.handshake" message when initialised', function() {
+    expect(window.top.postMessage).to.have.been.calledWithMatch({"key":"iframe.handshake","appGuid":appGuid});
+  });
+
+  describe.only('events', function() {
+
+    beforeEach(function() {
+      callback = sandbox.spy();
+    });
+
+    describe('#postMessage', function() {
+
+      it('waits until the client is ready to post messages', function() {
+        subject.postMessage('foo');
+        expect(window.top.postMessage).to.not.have.been.calledWithMatch({ "key": "foo" });
+        subject.trigger('app.registered');
+        expect(window.top.postMessage).to.have.been.calledWithMatch({ "key": "foo" });
+      });
+
+    });
+
+    describe('#on', function() {
+
+      it('registers a handler for a given event', function() {
+        subject.on('foo', callback);
+        expect(subject._messageHandlers['foo']).to.exist;
+      });
+
+      it('registers multiple handlers for the same event', function() {
+        subject.on('foo', callback);
+        subject.on('foo', callback);
+        expect(subject._messageHandlers['foo'].length).to.equal(2);
+      });
+
+      it('only registers when handler is a function', function() {
+        subject.on('foo', 2);
+        expect(subject._messageHandlers['foo']).to.not.exist;
+      });
+
+    });
+
+    describe('#off', function() {
+
+      it('removes a previously registered handler', function() {
+        subject.on('foo', callback);
+        expect(subject._messageHandlers['foo'].length).to.equal(1);
+        subject.off('foo', callback);
+        expect(subject._messageHandlers['foo'].length).to.equal(0);
+      });
+
+      it('returns the handler that was removed', function() {
+        subject.on('foo', callback);
+        expect(subject.off('foo', callback)).to.equal(callback);
+      });
+
+      it('returns false if no handler was found', function() {
+        expect(subject.off('foo', callback)).to.be.false;
+      });
+
+    });
+
+    describe('#has', function() {
+
+      it('returns true if the given handler is registered for the given event', function() {
+        subject.on('foo', callback);
+        expect(subject.has('foo', callback)).to.be.true;
+      });
+
+      it('returns false if the given handler is not registered for the given event', function() {
+        expect(subject.has('foo', callback)).to.be.false;
+      });
+
+      it("returns false if the given event isn't registered", function() {
+        expect(subject.has('bar')).to.be.false;
+      });
+
+    });
+
+    describe('#trigger', function() {
+
+      var data = {
+        bar: 2
+      };
+
+      it('returns false if no event handler is registered for the given event', function() {
+        expect(subject.trigger('foo')).to.be.false;
+      });
+
+      it('fires registered event handlers with any data provided', function() {
+        subject.on('foo', callback);
+        subject.on('foo', callback);
+        subject.trigger('foo', data);
+        expect(callback).to.have.been.calledWith(data);
+        expect(callback).to.have.been.calledTwice;
+      });
+
+    });
+
+  });
+
+});
