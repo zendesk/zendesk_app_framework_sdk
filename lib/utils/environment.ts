79b78bab27344f3d5a63e86a962a05d595f0152a
Christopher Goddard <cgoddard@zendesk.com>
2021-08-27 12:32:04 +1000

    WIP: adding idleState to the zaf_sdk

---
+++ lib/utils/environment.ts
@@ -0,0 +1,11 @@
+import currentVersion from './currentVersion';
+
+const NODE_ENV = process.env['NODE_ENV'];
+
+export const isProduction = NODE_ENV === 'production';
+
+export const isDevelopment = NODE_ENV === 'development';
+
+export const isTest = Boolean(NODE_ENV === 'test');
+
+export const isShaSpecified = currentVersion.specified;
