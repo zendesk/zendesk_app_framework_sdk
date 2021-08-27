0bb1ce6c1aaf024133e3f395df8fe7d84dbf9d6b
Christopher Goddard <cgoddard@zendesk.com>
2021-08-27 14:21:24 +1000

    Remove unnecessary code

--- lib/utils/environment.ts
+++ lib/utils/environment.ts
@@ -1,5 +1,3 @@
-import currentVersion from './currentVersion';
-
 const NODE_ENV = process.env['NODE_ENV'];
 
 export const isProduction = NODE_ENV === 'production';
@@ -7,5 +5,3 @@ export const isProduction = NODE_ENV === 'production';
 export const isDevelopment = NODE_ENV === 'development';
 
 export const isTest = Boolean(NODE_ENV === 'test');
-
-export const isShaSpecified = currentVersion.specified;
