0c4f9f25162b42ef1402d00e2cba72c36394b6f0
Christopher Goddard <cgoddard@zendesk.com>
2022-01-12 10:14:12 +1100

    Simplify determination as to whether we are in a test environment

--- lib/utils/environment.ts
+++ lib/utils/environment.ts
@@ -4,4 +4,4 @@ export const isProduction = NODE_ENV === 'production';
 
 export const isDevelopment = NODE_ENV === 'development';
 
-export const isTest = Boolean(NODE_ENV === 'test');
+export const isTest = NODE_ENV === 'test';
