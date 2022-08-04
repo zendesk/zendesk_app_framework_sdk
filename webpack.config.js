5d77355ef37092b62d82ffb4fa4c97a1071ee5f7
william xing <etanxing@gmail.com>
2022-08-04 12:01:11 +0930

    [VEG-1404] Keep the same output folder name

--- webpack.config.js
+++ webpack.config.js
@@ -46,13 +46,13 @@ const commonConfig = {
     },
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'build')
   },
 
-  // Note: devServer does not serve from dist/, but from cache. It also doesn't respect mode
+  // Note: devServer does not serve from build/, but from cache. It also doesn't respect mode
   // so outputed files are very different from server/build/build:dev
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'build'),
     hot: false,
     inline: false,
     compress: true,
